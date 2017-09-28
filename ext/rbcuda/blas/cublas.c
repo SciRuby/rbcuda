0c83dc099d1d220c5988c16b669906419a8846c6
prasun <prasunanand.bitsp@gmail.com>
2017-09-28 13:04:33 +0530

    void to VALUE

--- ext/rbcuda/blas/cublas.c
+++ ext/rbcuda/blas/cublas.c
@@ -76,81 +76,81 @@ static VALUE rb_cublasZdotc(VALUE self){
 
 /*------------------------------------------------------------------------*/
 /* SCAL */
-static void rb_cublasSscal(VALUE self){
+static VALUE rb_cublasSscal(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDscal(VALUE self){
+static VALUE rb_cublasDscal(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCscal(VALUE self){
+static VALUE rb_cublasCscal(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZscal(VALUE self){
+static VALUE rb_cublasZscal(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCsscal(VALUE self){
+static VALUE rb_cublasCsscal(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZdscal(VALUE self){
+static VALUE rb_cublasZdscal(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* AXPY */
-static void rb_cublasSaxpy(VALUE self){
+static VALUE rb_cublasSaxpy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDaxpy(VALUE self){
+static VALUE rb_cublasDaxpy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCaxpy(VALUE self){
+static VALUE rb_cublasCaxpy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZaxpy(VALUE self){
+static VALUE rb_cublasZaxpy(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* COPY */
-static void rb_cublasScopy(VALUE self){
+static VALUE rb_cublasScopy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDcopy(VALUE self){
+static VALUE rb_cublasDcopy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCcopy(VALUE self){
+static VALUE rb_cublasCcopy(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZcopy(VALUE self){
+static VALUE rb_cublasZcopy(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* SWAP */
-static void rb_cublasSswap(VALUE self){
+static VALUE rb_cublasSswap(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDswap(VALUE self){
+static VALUE rb_cublasDswap(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCswap(VALUE self){
+static VALUE rb_cublasCswap(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZswap(VALUE self){
+static VALUE rb_cublasZswap(VALUE self){
   return Qnil;
 }
 
@@ -209,57 +209,57 @@ static VALUE rb_cublasDzasum(VALUE self){
 
 /*------------------------------------------------------------------------*/
 /* ROT */
-static void rb_cublasSrot(VALUE self){
+static VALUE rb_cublasSrot(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDrot(VALUE self){
+static VALUE rb_cublasDrot(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCrot(VALUE self){
+static VALUE rb_cublasCrot(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZrot(VALUE self){
+static VALUE rb_cublasZrot(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCsrot(VALUE self){
+static VALUE rb_cublasCsrot(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZdrot(VALUE self){
+static VALUE rb_cublasZdrot(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* ROTG */
-static void rb_cublasSrotg(VALUE self){
+static VALUE rb_cublasSrotg(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDrotg(VALUE self){
+static VALUE rb_cublasDrotg(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCrotg(VALUE self){
+static VALUE rb_cublasCrotg(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZrotg(VALUE self){
+static VALUE rb_cublasZrotg(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* ROTM */
-static void rb_cublasSrotm(VALUE self){
+static VALUE rb_cublasSrotm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDrotm(VALUE self){
+static VALUE rb_cublasDrotm(VALUE self){
   return Qnil;
 }
 
@@ -267,11 +267,11 @@ static void rb_cublasDrotm(VALUE self){
 
 /*------------------------------------------------------------------------*/
 /* ROTMG */
-static void rb_cublasSrotmg(VALUE self){
+static VALUE rb_cublasSrotmg(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDrotmg(VALUE self){
+static VALUE rb_cublasDrotmg(VALUE self){
   return Qnil;
 }
 
@@ -279,56 +279,56 @@ static void rb_cublasDrotmg(VALUE self){
 
 /* --------------- CUBLAS BLAS2 functions  ---------------- */
 /* GEMV */
-static void rb_cublasSgemv(VALUE self){
+static VALUE rb_cublasSgemv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDgemv(VALUE self){
+static VALUE rb_cublasDgemv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCgemv(VALUE self){
+static VALUE rb_cublasCgemv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZgemv(VALUE self){
+static VALUE rb_cublasZgemv(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* GBMV */
-static void rb_cublasSgbmv(VALUE self){
+static VALUE rb_cublasSgbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDgbmv(VALUE self){
+static VALUE rb_cublasDgbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCgbmv(VALUE self){
+static VALUE rb_cublasCgbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZgbmv(VALUE self){
+static VALUE rb_cublasZgbmv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TRMV */
-static void rb_cublasStrmv(VALUE self){
+static VALUE rb_cublasStrmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtrmv(VALUE self){
+static VALUE rb_cublasDtrmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtrmv(VALUE self){
+static VALUE rb_cublasCtrmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtrmv(VALUE self){
+static VALUE rb_cublasZtrmv(VALUE self){
   return Qnil;
 }
 
@@ -336,292 +336,292 @@ static void rb_cublasZtrmv(VALUE self){
 
 /*------------------------------------------------------------------------*/
 /* TBMV */
-static void rb_cublasStbmv(VALUE self){
+static VALUE rb_cublasStbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtbmv(VALUE self){
+static VALUE rb_cublasDtbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtbmv(VALUE self){
+static VALUE rb_cublasCtbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtbmv(VALUE self){
+static VALUE rb_cublasZtbmv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TPMV */
-static void rb_cublasStpmV(VALUE self){
+static VALUE rb_cublasStpmV(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtpmv(VALUE self){
+static VALUE rb_cublasDtpmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtpmv(VALUE self){
+static VALUE rb_cublasCtpmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtpmv(VALUE self){
+static VALUE rb_cublasZtpmv(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* TRSV */
-static void rb_cublasStrsv(VALUE self){
+static VALUE rb_cublasStrsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtrsv(VALUE self){
+static VALUE rb_cublasDtrsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtrsv(VALUE self){
+static VALUE rb_cublasCtrsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtrsv(VALUE self){
+static VALUE rb_cublasZtrsv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TPSV */
-static void rb_cublasStpsv(VALUE self){
+static VALUE rb_cublasStpsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtpsv(VALUE self){
+static VALUE rb_cublasDtpsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtpsv(VALUE self){
+static VALUE rb_cublasCtpsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtpsv(VALUE self){
+static VALUE rb_cublasZtpsv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TBSV */
-static void rb_cublasStbsv(VALUE self){
+static VALUE rb_cublasStbsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtbsv(VALUE self){
+static VALUE rb_cublasDtbsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtbsv(VALUE self){
+static VALUE rb_cublasCtbsv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtbsv(VALUE self){
+static VALUE rb_cublasZtbsv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SYMV/HEMV */
-static void rb_cublasSsymv(VALUE self){
+static VALUE rb_cublasSsymv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsymv(VALUE self){
+static VALUE rb_cublasDsymv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasChemv(VALUE self){
+static VALUE rb_cublasChemv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhemv(VALUE self){
+static VALUE rb_cublasZhemv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SBMV/HBMV */
-static void rb_cublasSsbmv(VALUE self){
+static VALUE rb_cublasSsbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsbmv(VALUE self){
+static VALUE rb_cublasDsbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasChbmv(VALUE self){
+static VALUE rb_cublasChbmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhbmv(VALUE self){
+static VALUE rb_cublasZhbmv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SPMV/HPMV */
-static void rb_cublasSspmv(VALUE self){
+static VALUE rb_cublasSspmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDspmv(VALUE self){
+static VALUE rb_cublasDspmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasChpmv(VALUE self){
+static VALUE rb_cublasChpmv(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhpmv(VALUE self){
+static VALUE rb_cublasZhpmv(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* GER */
-static void rb_cublasSger(VALUE self){
+static VALUE rb_cublasSger(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDger(VALUE self){
+static VALUE rb_cublasDger(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCgeru(VALUE self){
+static VALUE rb_cublasCgeru(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCgerc(VALUE self){
+static VALUE rb_cublasCgerc(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZgeru(VALUE self){
+static VALUE rb_cublasZgeru(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZgerc(VALUE self){
+static VALUE rb_cublasZgerc(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SYR/HER */
-static void rb_cublasSsyr(VALUE self){
+static VALUE rb_cublasSsyr(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsyr(VALUE self){
+static VALUE rb_cublasDsyr(VALUE self){
   return Qnil;
 }
 
 
-static void rb_cublasCher(VALUE self){
+static VALUE rb_cublasCher(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZher(VALUE self){
+static VALUE rb_cublasZher(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SPR/HPR */
-static void rb_cublasSspr(VALUE self){
+static VALUE rb_cublasSspr(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDspr(VALUE self){
+static VALUE rb_cublasDspr(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasChpr(VALUE self){
+static VALUE rb_cublasChpr(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhpr(VALUE self){
+static VALUE rb_cublasZhpr(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SYR2/HER2 */
-static void rb_cublasSsyr2(VALUE self){
+static VALUE rb_cublasSsyr2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsyr2(VALUE self){
+static VALUE rb_cublasDsyr2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCher2(VALUE self){
+static VALUE rb_cublasCher2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZher2(VALUE self){
+static VALUE rb_cublasZher2(VALUE self){
   return Qnil;
 }
 
 /*------------------------------------------------------------------------*/
 /* SPR2/HPR2 */
-static void rb_cublasSspr2(VALUE self){
+static VALUE rb_cublasSspr2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDspr2(VALUE self){
+static VALUE rb_cublasDspr2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasChpr2(VALUE self){
+static VALUE rb_cublasChpr2(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhpr2(VALUE self){
+static VALUE rb_cublasZhpr2(VALUE self){
   return Qnil;
 }
 
 
 /* ------------------------BLAS3 Functions ------------------------------- */
 /* GEMM */
-static void rb_cublasSgemm(VALUE self){
+static VALUE rb_cublasSgemm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDgemm(VALUE self){
+static VALUE rb_cublasDgemm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCgemm(VALUE self){
+static VALUE rb_cublasCgemm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZgemm(VALUE self){
+static VALUE rb_cublasZgemm(VALUE self){
   return Qnil;
 }
 
 /* -------------------------------------------------------*/
 /* SYRK */
 
-static void rb_cublasSsyrk(VALUE self){
+static VALUE rb_cublasSsyrk(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsyrk(VALUE self){
+static VALUE rb_cublasDsyrk(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCsyrk(VALUE self){
+static VALUE rb_cublasCsyrk(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZsyrk(VALUE self){
+static VALUE rb_cublasZsyrk(VALUE self){
   return Qnil;
 }
 
@@ -629,11 +629,11 @@ static void rb_cublasZsyrk(VALUE self){
 
 /* ------------------------------------------------------- */
 /* HERK */
-static void rb_cublasCherk(VALUE self){
+static VALUE rb_cublasCherk(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZherk(VALUE self){
+static VALUE rb_cublasZherk(VALUE self){
   return Qnil;
 }
 
@@ -641,96 +641,96 @@ static void rb_cublasZherk(VALUE self){
 
 /* ------------------------------------------------------- */
 /* SYR2K */
-static void rb_cublasSsyr2k(VALUE self){
+static VALUE rb_cublasSsyr2k(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsyr2k(VALUE self){
+static VALUE rb_cublasDsyr2k(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCsyr2k(VALUE self){
+static VALUE rb_cublasCsyr2k(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZsyr2k(VALUE self){
+static VALUE rb_cublasZsyr2k(VALUE self){
   return Qnil;
 }
 
 /* ------------------------------------------------------- */
 /* HER2K */
-static void rb_cublasCher2k(VALUE self){
+static VALUE rb_cublasCher2k(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZher2k(VALUE self){
+static VALUE rb_cublasZher2k(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* SYMM*/
-static void rb_cublasSsymm(VALUE self){
+static VALUE rb_cublasSsymm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDsymm(VALUE self){
+static VALUE rb_cublasDsymm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCsymm(VALUE self){
+static VALUE rb_cublasCsymm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZsymm(VALUE self){
+static VALUE rb_cublasZsymm(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* HEMM*/
-static void rb_cublasChemm(VALUE self){
+static VALUE rb_cublasChemm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZhemm(VALUE self){
+static VALUE rb_cublasZhemm(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TRSM*/
-static void rb_cublasStrsm(VALUE self){
+static VALUE rb_cublasStrsm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtrsm(VALUE self){
+static VALUE rb_cublasDtrsm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtrsm(VALUE self){
+static VALUE rb_cublasCtrsm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtrsm(VALUE self){
+static VALUE rb_cublasZtrsm(VALUE self){
   return Qnil;
 }
 
 
 /*------------------------------------------------------------------------*/
 /* TRMM*/
-static void rb_cublasStrmm(VALUE self){
+static VALUE rb_cublasStrmm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasDtrmm(VALUE self){
+static VALUE rb_cublasDtrmm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasCtrmm(VALUE self){
+static VALUE rb_cublasCtrmm(VALUE self){
   return Qnil;
 }
 
-static void rb_cublasZtrmm(VALUE self){
+static VALUE rb_cublasZtrmm(VALUE self){
   return Qnil;
 }
