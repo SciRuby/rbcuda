d50749c07ef27dd9d1792f249e4909eb54a1a021
prasun <prasunanand.bitsp@gmail.com>
2017-09-30 01:54:49 +0530

    modify matmul example

--- examples/matmul.rb
+++ examples/matmul.rb
@@ -9,7 +9,7 @@ class ResultCollect
 
   def self.generate
 
-    iters = 0
+    iters = 5
     result = {}
 
     result[:mat_mult] = []
@@ -21,27 +21,42 @@ class ResultCollect
                 ]
 
     shapeArray.each do |shape|
-      cpu_ary1 = Array.new(shape[0]*shape[1]) { rand(1...999999) }
-      cpu_ary2 = Array.new(shape[0]*shape[1]) { rand(1...999999) }
+      puts "shape"+shape.to_s
+      cpu_ary1 = Array.new(shape[0]*shape[1]) { 2 }
+      cpu_ary2 = Array.new(shape[0]*shape[1]) { 2 }
+      cpu_ary_res = []
 
 
-      gpu_ary1    = cudaMalloc(shape[0] * shape[1]);
-      gpu_ary2    = cudaMalloc(shape[0] * shape[1]);
-      gpu_ary_res = cudaMalloc(shape[0] * shape[1]);
+      gpu_ary1    = RbCUDA::Runtime.cudaMalloc(shape);
+      gpu_ary2    = RbCUDA::Runtime.cudaMalloc(shape);
+      gpu_ary_res = RbCUDA::Runtime.cudaMalloc(shape);
 
-      cudaMemcpy(gpu_ary1, cpu_ary1, shape[0]*shape[1], :cudaMemcpyHostToDevice);
-      cudaMemcpy(gpu_ary2, cpu_ary2, shape[0]*shape[1], :cudaMemcpyHostToDevice);
+      RbCUDA::Runtime.cudaMemcpy(gpu_ary1, cpu_ary1, shape[0]*shape[1], :cudaMemcpyHostToDevice);
+      RbCUDA::Runtime.cudaMemcpy(gpu_ary2, cpu_ary2, shape[0]*shape[1], :cudaMemcpyHostToDevice);
 
-      gpu_blas_mmul(gpu_ary1, gpu_ary2, gpu_ary_res, shape[0], shape[1], shape[1]);
+      lda= m = shape[0]
+      ldb= n = shape[0]
+      ldc= k = shape[0]
+      alf = 1
+      bet = 0
+      alpha = alf
+      beta = bet
+      handle = RbCUDA::CuBLAS_v2.cublasCreate_v2()
+      iters.times do
+        puts Benchmark.measure{
 
-      cudaMemcpy(cpu_ary_res, gpu_ary_res, shape[0]*shape[1], :cudaMemcpyDeviceToHost);
-
-      cudaFree(gpu_ary1)
-      cudaFree(gpu_ary2)
-      cudaFree(gpu_ary_res)
+          RbCUDA::CuBLAS_v2.cublasDgemm_v2(handle, :CUBLAS_OP_N, :CUBLAS_OP_N, m, n, k, alpha, gpu_ary1, lda, gpu_ary2, ldb, beta, gpu_ary_res, ldc)
+        }
+      end
+      # puts RbCUDA::Runtime.cudaMemcpy([], gpu_ary_res, shape[0]*shape[1], :cudaMemcpyDeviceToHost);
+      RbCUDA::CuBLAS_v2.cublasDestroy_v2(handle)
+      RbCUDA::Runtime.cudaFree(gpu_ary1)
+      RbCUDA::Runtime.cudaFree(gpu_ary2)
+      RbCUDA::Runtime.cudaFree(gpu_ary_res)
     end
 
   end
+
 end
 
 ResultCollect.generate
